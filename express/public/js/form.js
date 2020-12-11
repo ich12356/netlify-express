a39a30de2f2d17ac7085d96653704dd391eddec3
Tom Wawerek <tw64cyty@studserv.uni-leipzig.de>
2020-12-11 21:10:13 +0100

    .

--- express/public/js/form.js
+++ express/public/js/form.js
@@ -7,9 +7,13 @@ form.addEventListener("submit", (e) => {
 	if (input.value.trim() == "") return;
 
 	const XHR = new XMLHttpRequest();
+	XHR.addEventListener("error", (e) => {
+		console.log(e);
+	});
 	XHR.open("POST", "/.netlify/functions/server/addEmail");
 	XHR.setRequestHeader("Content-Type", "application/json");
 	XHR.send(JSON.stringify({ email: input.value.trim() }));
+	XHR.abort();
 
 	input.value = "";
 });
